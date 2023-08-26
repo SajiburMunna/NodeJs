c64ea521bd9a16ec5900f18ac637477b9946cf1a
sajiburmunna <sajiburmunna087@gmail.com>
2023-08-26 23:41:43 +0600

    chore:lodash init

--- fileRead.js
+++
@@ -1,9 +0,0 @@
-const fs=require('fs')
-
-fs.readFile('./test.json',(err,data)=>{
-    if(err){
-        console.log(err)
-    }
-    const mainData = JSON.parse(data)
-    console.log(mainData)
-})
--- global.js
+++
@@ -1,7 +0,0 @@
-//  const math =require('./math');
- const {sum} =require('./math');
-
- const os=require('os')
- console.log(os.arch())
-
- console.log(sum(5,5));
